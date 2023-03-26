fc4bb0573da2bc3b20fe1e756e5d9499afbb4847
Neto <franciscomdlneto@gmail.com>
2023-03-26 15:15:03 -0300

    estudo de tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //String
+        String meuNome = "Francisco Neto";
+        byte idade = 30;
+        short ano = 2023;
+        int cep = 21070333;//cep qualquer
+        long cpf = 98765432109l;//cpf ilustrativo
+        float pi = 3.14f;
+        final float VALOR_DE_PI = 3.14f;
+        double salario = 1825.33;//salário ilustrativo
+        /*teste de atribuição de valores entre variáveis
+         de diferentes tipos*/
+        int numeroA = 10;
+        int numeroB = 3;
+        /*Neste exemplo eu tenho que tipar a entrada da variável
+         * divisao para que o Java atribua o valor da operação
+         */
+        double divisao = (double) numeroA/numeroB;
+        System.out.println("Resultado: "+divisao);
+        
     }
 }
